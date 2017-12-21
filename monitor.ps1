3f31ed730a57e4d9c5d5ebdad9c64441f2897668
Louis Machin <ldhmachin@gmail.com>
2017-12-21 11:57:42 +0000

    Update monitor.ps1

--- monitor.ps1
+++ monitor.ps1
@@ -6,7 +6,7 @@ Function Register-Monitor {
         EnableRaisingEvents = $true
     }
     $changeAction = [scriptblock]::Create('
-        # This is the code which will be executed every time a file change is detected
+        # Executed on file change
         $path = $Event.SourceEventArgs.FullPath
         $name = $Event.SourceEventArgs.Name
         $changeType = $Event.SourceEventArgs.ChangeType
